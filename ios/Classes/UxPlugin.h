810d060d44d1c181c7b16d1b155ea5ffd4163ea5
agrapine <alexandru@agrapine.com>
2019-08-07 15:37:44 +0100

    red pill

---
+++ ios/Classes/UxPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface UxPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/UxPlugin.m
@@ -0,0 +1,20 @@
+#import "UxPlugin.h"
+
+@implementation UxPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"ux"
+            binaryMessenger:[registrar messenger]];
+  UxPlugin* instance = [[UxPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
